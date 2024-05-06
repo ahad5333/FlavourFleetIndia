ea3df97cd510efb22aa48b8a39b0e4e561b13317
Md Ahadullah <ahad53344@gmail.com>
2024-05-06 14:37:00 +0530

    added food items

--- frontEnd/src/pages/Home/Home.jsx
+++ frontEnd/src/pages/Home/Home.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import "./Home.css"
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay"
+
 
 
 function Home() {
@@ -11,6 +13,7 @@ function Home() {
       <Header />
       <ExploreMenu category= {category}
       setCategory= {setCategory}/>
+      <FoodDisplay category = {category} />
     </div>
   )
 }
